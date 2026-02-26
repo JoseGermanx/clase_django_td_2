65f5ba486e7e44a0833e861f116da3ada8a74548
José Martínez <jgermanmm@gmail.com>
2026-02-26 20:47:43 -0300

    404

--- demo_project/settings.py
+++ demo_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@j&!+7cjq%qxjg@ue8i%g=c9c@&iqjq9ezabkq=g@yxq6bu(x#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
